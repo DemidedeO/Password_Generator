681d5032e02f5cdb4bddf3fc3ac23935eda2644e
DemidedeO <demi.oladipo@gmail.com>
2020-03-22 21:14:52 -0400

    updated copy

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var helperText = {
-  charLength: document.querySelector('.helper-text .length'),
-  lowercase: document.querySelector('.helper-text .lowercase'),
-  uppercase: document.querySelector('.helper-text .uppercase'),
-  special: document.querySelector('.helper-text .special')
-};
+//var generateBtn = document.querySelector("#generate")
+//var helperText = {
+ // charLength: document.querySelector('.helper-text .length'),
+ // lowercase: document.querySelector('.helper-text .lowercase'),
+ // uppercase: document.querySelector('.helper-text .uppercase'),
+  //special: document.querySelector('.helper-text .special')
+//};
+
 
 var password = {
-  lowercase: 'abcdefghijklmnopqrstuvwxyz',
-  Uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  characters: '!@#$%^&*()<>./?`',
-  number: '012345678910'
-
+  lowercase: ["abcdefghijklmnopqrstuvwxyz"],
+  Uppercase: ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+  characters: ["!@#$%^&*()<>./?`"],
+  number: ["0123456789"],
 }
-var confirmlength = confirm("Password will be 8 to 128 characters")
-var lowercasechoice = prompt("Would you like to include lowercase?")
-var Uppercasechoice = prompt("would you like to include Uppercase?")
-var question4 = prompt("would you like to include special characters?")
-var question5 = prompt("would you like to include numbers?")
 
-//if (confirmlength) {
- // alert("");
-
-// Write password to the #password input
 function writePassword() {
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -33,5 +23,21 @@ function writePassword() {
 
 }
 
+var confirmlength = confirm("Password will be 8 to 128 characters")
+var lowercasechoice = prompt("Would you like to include lowercase?")
+var Uppercasechoice = prompt("would you like to include Uppercase?")
+var question4 = prompt("would you like to include special characters?")
+var question5 = prompt("would you like to include numbers?")
+
+//if (lowercasechoice = "no") {
+ // alert("");
+
+// Write password to the #password input
+function writePassword() {
+
+
+
+}
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
